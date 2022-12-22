eddc85e832ae63dbee749b48aad0587ab0f51091
SHXiao-Stella <42401672+SHXiao-Stella@users.noreply.github.com>
2022-12-22 15:00:57 +0800

    Add files via upload

---
+++ Re_word2pdf.py
@@ -0,0 +1,31 @@
+#在前人代码（https://www.jianshu.com/p/fed949abe811）的基础上修正了“被呼叫方拒绝接收呼叫”问题
+
+from win32com.client import constants,gencache
+import os
+import time
+
+def Word_to_Pdf(Word_path,Pdf_path): # Word转pdf方法,第一个参数代表word文档路径，第二个参数代表pdf文档路径
+    word = gencache.EnsureDispatch('Word.Application')
+    word.Visible = 0  #不显示
+    doc = word.Documents.Open(Word_path,ReadOnly = 1)
+    doc.ExportAsFixedFormat(Pdf_path,constants.wdExportFormatPDF)
+    word.Documents.Close(SaveChanges=0)
+    word.Quit()
+    
+
+Word_files = []
+for file in os.listdir('.'):
+    if file.endswith(('.doc','.docx')):
+        Word_files.append(file)
+
+count=0
+for file in Word_files:
+    file_path = os.path.abspath(file)
+    index = file_path.rindex('.')
+    pdf_path = file_path[:index] + '.pdf'
+    print(pdf_path)
+    #time.sleep(4)
+    Word_to_Pdf(file_path,pdf_path)
+    count +=1
+    if count % 100 == 0: #每转换100个print1次
+        print 'did 100'
