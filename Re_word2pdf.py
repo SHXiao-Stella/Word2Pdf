b2e44a44f672485fdfb1892d1c8f50155cefdc67
SHXiao-Stella <42401672+SHXiao-Stella@users.noreply.github.com>
2022-12-23 12:30:16 +0800

    Update Re_word2pdf.py

--- Re_word2pdf.py
+++ Re_word2pdf.py
@@ -1,6 +1,6 @@
 #在前人代码（https://www.jianshu.com/p/fed949abe811）的基础上修正了“被呼叫方拒绝接收呼叫”问题
 
-from win32com.client import constants,gencache
+from win32com.client import constants,gencache,pywintypes
 import os
 import time
 
@@ -17,15 +17,25 @@ Word_files = []
 for file in os.listdir('.'):
     if file.endswith(('.doc','.docx')):
         Word_files.append(file)
-
+#or
+#path='D:\\文件夹'
+#for file in os.listdir(path):
+#    if file.endswith(('.doc','.docx')):
+#        Word_files.append(file)
+        
+        
 count=0
 for file in Word_files:
-    file_path = os.path.abspath(file)
-    index = file_path.rindex('.')
-    pdf_path = file_path[:index] + '.pdf'
-    print(pdf_path)
-    #time.sleep(4)
-    Word_to_Pdf(file_path,pdf_path)
-    count +=1
-    if count % 100 == 0: #每转换100个print1次
-        print 'did 100'
+    count += 1
+    try:
+        file_path = os.path.abspath(file)
+        index = file_path.rindex('.')
+        pdf_path = file_path[:index] + '.pdf'
+        Word_to_Pdf(file_path, pdf_path)
+        print(count)
+        time.sleep(4)
+        #if count % 100 == 0: #每转换100个print1次
+        #    print 'did 100'
+    except pywintypes.com_error:
+        print(pdf_path)
+        continue
